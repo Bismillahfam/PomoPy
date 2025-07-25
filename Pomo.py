40e9e8fbdb15ca5bb51e60d0af4d5bf6ae95e29d
Muhammad Ahsan Malik <ahsanmaleek1.27@gmail.com>
2025-07-25 19:29:41 +0000

    Window

---
+++ Pomo.py
@@ -0,0 +1,11 @@
+# This is a basic pomodoro timer application in Python.
+
+# Import necessary libraries
+import datetime as dt, math, json, tkinter as tk
+
+# Intitialise the window
+window = tk.Tk()
+window.title("Pomodoro Timer")
+window.geometry("400x300")
+
+window.mainloop()
